f7d354057e68453adb559e8327977dea5990eda3
ukijo <nes-you-ukijo@hotmail.fr>
2023-08-28 23:09:21 +0100

    delete some unused code

--- fireball/src/components/Body.jsx
+++ fireball/src/components/Body.jsx
@@ -1,7 +1,7 @@
 import Header from "./Header";
 import Footer from "./Footer";
 // import Map from "./Map";
-Map;
+
 import Summary from "./summary/Summary";
 import Table from "./Table";
 import Map from "./map/Map";

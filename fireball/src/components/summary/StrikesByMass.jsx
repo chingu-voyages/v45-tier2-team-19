ec3fe23eb7c2215a4a583aa0efe657a975a85ea0
crisxh <76125739+crisxh@users.noreply.github.com>
2023-08-29 08:14:57 +0200

    list of top 10

--- fireball/src/components/summary/StrikesByMass.jsx
+++ fireball/src/components/summary/StrikesByMass.jsx
@@ -4,40 +4,46 @@ const StrikesByMass = function () {
     const meteorData = useDataContext().data;
 
     useEffect(() => {
-        let largestArray = []
-        let smallestArray = []
+
         console.log('meteorData', meteorData)
-        meteorData.forEach((meteor) => {
-            if (meteor['mass (g)'] > 500000) {
-                largestArray.push(meteor['mass (g)'])
-            }
 
-            if (meteor['mass (g)'] < .1 && meteor['mass (g)'] && meteor['mass (g)'] != "0") {
-                smallestArray.push(meteor['mass (g)'])
-            }
-
-
-        })
         function sort(a, b) {
             return a - b
         }
 
-        largestArray.sort(function sort(a, b) {
-            return b - a
-        })
-
-        console.log(largestArray)
-        console.log('smallest', smallestArray)
-
-        let topTen = largestArray.slice(0, 10)
-        console.log('topTen', topTen)
-
     }, [])
 
+    let largestArray = []
+    let smallestArray = []
+    meteorData.forEach((meteor) => {
+        if (meteor['mass (g)'] > 500000) {
+            largestArray.push(meteor['mass (g)'])
+        }
+
+        if (meteor['mass (g)'] < .1 && meteor['mass (g)'] && meteor['mass (g)'] != "0") {
+            smallestArray.push(meteor['mass (g)'])
+        }
+
+
+    })
+
+    largestArray.sort(function sort(a, b) {
+        return b - a
+    })
+    console.log(largestArray)
+    console.log('smallest', smallestArray)
+
+    let topTen = largestArray.slice(0, 10)
+    console.log('topTen', topTen)
+
+    const topTenList = topTen.map((meteor, index) => {
+        return <p key={index}>{meteor}</p>
+    })
 
     return (
         <div>
             <h1>Strikes By Mass</h1>
+            {topTenList}
         </div>
     )
 }

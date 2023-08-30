8d1e3f88383c4e12fb010a6365260c5b4722a05e
crisxh <76125739+crisxh@users.noreply.github.com>
2023-08-30 21:41:08 +0200

    added github links

--- fireball/src/components/layout/Footer.jsx
+++ fireball/src/components/layout/Footer.jsx
@@ -2,33 +2,35 @@ import logo from "../../assets/fireballLogo.jpg";
 import "./Footer.css";
 
 const Footer = function () {
-  return (
-    <footer>
-      <div id="logo">
-        <img className="logo" src={logo} alt="header logo" />
-      </div>
-      <div id="madeBy">
-        <h2>App Created By:</h2>
-        <ul>
-          <a href="">
-            <li>Alex</li>
-          </a>
-          <a href="">
-            <li>Crystal</li>
-          </a>
-          <a href="">
-            <li>Chris</li>
-          </a>
-          <a href="">
-            <li>Younes</li>
-          </a>
-          <a href="">
-            <li>Sarita</li>
-          </a>
-        </ul>
-      </div>
-    </footer>
-  );
+    return (
+        <footer>
+            <div id="logo">
+                <img className="logo" src={logo} alt="header logo" />
+            </div>
+            <div id="madeBy">
+                <h2>App Created By:</h2>
+                <ul>
+                    <a href="https://github.com/vukas86">
+                        <li>Alex</li>
+                    </a>
+
+                    <a href="https://github.com/crwainstock">
+                        <li>Crystal</li>
+                    </a>
+
+                    <a href="https://github.com/crisxh">
+                        <li>Chris</li>
+                    </a>
+                    <a href="https://github.com/uKiJo">
+                        <li>Younes</li>
+                    </a>
+                    <a href="https://github.com/Sarita1517">
+                        <li>Sarita</li>
+                    </a>
+                </ul>
+            </div>
+        </footer>
+    );
 };
 
 export default Footer;

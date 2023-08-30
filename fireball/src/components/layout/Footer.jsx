429213f71ef9988dd9d74175f5acac0286d26793
crisxh <76125739+crisxh@users.noreply.github.com>
2023-08-30 16:07:05 +0200

    attept to add react-icons to footer

--- fireball/src/components/layout/Footer.jsx
+++ fireball/src/components/layout/Footer.jsx
@@ -1,8 +1,13 @@
-import logo from '../../assets/fireballLogo.jpg'
+import logo from '../../assets/fireballLogo.jpg';
+import { FaLinkedIn } from 'react-icons/fa6';
+
+
+
 const Footer = function () {
     return (
 
         <footer>
+
             <div id='logo'>
                 <img className="logo" src={logo} alt="header logo" />
             </div>
@@ -12,9 +17,11 @@ const Footer = function () {
                     <a href="">
                         <li>Alex</li>
                     </a>
+
                     <a href="">
                         <li>Crystal</li>
                     </a>
+                    <FaLinkedIn />
                     <a href="">
                         <li>Chris</li>
                     </a>

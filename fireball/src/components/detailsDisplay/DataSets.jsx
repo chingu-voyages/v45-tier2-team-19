24523bb34d3c96c41c5c65643484c2ec9759c7aa
“vukas86” <vukasbit@gmail.com>
2023-08-30 12:56:56 +0200

    minor changes

--- fireball/src/components/detailsDisplay/DataSets.jsx
+++ fireball/src/components/detailsDisplay/DataSets.jsx
@@ -1,4 +1,3 @@
-import styles from "./DataSets.module.css";
 import { useEffect, useMemo, useState } from "react";
 import { useTable, useSortBy, useFilters, usePagination } from "react-table";
 import { useDataContext } from "../../hooks/useDataContext";
@@ -13,28 +12,6 @@ function DataSets() {
 
   const [isLoading, setIsLoading] = useState(true);
   const [formattedData, setFormattedData] = useState([]);
-
-  // const fetchLocationData = async (latitude, longitude) => {
-  //   const API_KEY = "SQ2wqGDLvUAL1TEwaOucCOLQaAs81Eto";
-
-  //   if (latitude && longitude && API_KEY) {
-  //     const apiUrl = `https://api.tomtom.com/search/2/reverseGeocode/${latitude},${longitude}.json?key=${API_KEY}`;
-
-  //     try {
-  //       const response = await fetch(apiUrl);
-  //       if (response.ok) {
-  //         const data = await response.json();
-  //         return data;
-  //       } else {
-  //         console.error("API request failed");
-  //       }
-  //     } catch (error) {
-  //       console.error("Error:", error);
-  //     }
-  //   }
-
-  //   return null;
-  // };
 
   useEffect(() => {
     const formatted = originalData.map((item) => ({

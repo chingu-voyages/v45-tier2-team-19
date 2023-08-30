177d8ed72ff57ef345033b1682a9c14699fb2f58
crisxh <76125739+crisxh@users.noreply.github.com>
2023-08-30 16:58:03 +0200

    added github links to footer

--- fireball/src/components/layout/Footer.jsx
+++ fireball/src/components/layout/Footer.jsx
@@ -14,21 +14,21 @@ const Footer = function () {
             <div id='madeBy'>
                 <h2>App Created By:</h2>
                 <ul>
-                    <a href="">
+                    <a href="https://github.com/vukas86">
                         <li>Alex</li>
                     </a>
 
-                    <a href="">
+                    <a href="https://github.com/crwainstock">
                         <li>Crystal</li>
                     </a>
 
-                    <a href="">
+                    <a href="https://github.com/crisxh">
                         <li>Chris</li>
                     </a>
-                    <a href="">
+                    <a href="https://github.com/uKiJo">
                         <li>Younes</li>
                     </a>
-                    <a href="">
+                    <a href="https://github.com/Sarita1517">
                         <li>Sarita</li>
                     </a>
                 </ul>

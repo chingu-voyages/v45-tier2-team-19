3fa98982409aa0dd6836487ffa60ab29aec0cde3
“vukas86” <vukasbit@gmail.com>
2023-08-30 10:45:01 +0200

    making minor changes with the background

--- fireball/src/components/detailsDisplay/DataSets.jsx
+++ fireball/src/components/detailsDisplay/DataSets.jsx
@@ -86,92 +86,97 @@ function DataSets() {
   const { pageIndex } = state;
 
   return (
-    <div>
-      {isLoading ? (
-        <div className="custom-loader"></div>
-      ) : (
-        <>
-          <table {...getTableProps()}>
-            <thead>
-              {headerGroups.map((headerGroup) => (
-                <>
-                  <tr {...headerGroup.getHeaderGroupProps()}>
-                    {headerGroup.headers.map((column) => (
-                      <th
-                        key={column.id}
-                        {...column.getHeaderProps(
-                          column.getSortByToggleProps()
-                        )}
-                      >
-                        {column.render("Header")}
-                        <div>
-                          {column.canFilter ? column.render("Filter") : null}
-                        </div>
-                      </th>
-                    ))}
-                  </tr>
-                </>
-              ))}
-            </thead>
-            <tbody {...getTableBodyProps()}>
-              {page.map((row) => {
-                prepareRow(row);
-                return (
-                  <tr key={row.id} {...row.getRowProps()}>
-                    {row.cells.map((cell) => {
-                      return (
-                        <td key={cell.column.id} {...cell.getCellProps()}>
-                          {cell.render("Cell")}
-                        </td>
-                      );
-                    })}
-                  </tr>
-                );
-              })}
-            </tbody>
-          </table>
-          <div className="paginationContainer">
-            <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
-              {"<<"}
-            </button>
-            <button onClick={() => previousPage()} disabled={!canPreviousPage}>
-              Previous
-            </button>
-            <span className="pageNumbers">
-              Page{" "}
-              <strong>
-                {pageIndex + 1} of {pageOptions.length}
-              </strong>{" "}
-            </span>
+    <section className={styles.datasetsContainer}>
+      <div>
+        {isLoading ? (
+          <div className="custom-loader"></div>
+        ) : (
+          <>
+            <table {...getTableProps()}>
+              <thead>
+                {headerGroups.map((headerGroup) => (
+                  <>
+                    <tr {...headerGroup.getHeaderGroupProps()}>
+                      {headerGroup.headers.map((column) => (
+                        <th
+                          key={column.id}
+                          {...column.getHeaderProps(
+                            column.getSortByToggleProps()
+                          )}
+                        >
+                          {column.render("Header")}
+                          <div>
+                            {column.canFilter ? column.render("Filter") : null}
+                          </div>
+                        </th>
+                      ))}
+                    </tr>
+                  </>
+                ))}
+              </thead>
+              <tbody {...getTableBodyProps()}>
+                {page.map((row) => {
+                  prepareRow(row);
+                  return (
+                    <tr key={row.id} {...row.getRowProps()}>
+                      {row.cells.map((cell) => {
+                        return (
+                          <td key={cell.column.id} {...cell.getCellProps()}>
+                            {cell.render("Cell")}
+                          </td>
+                        );
+                      })}
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+            <div className="paginationContainer">
+              <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+                {"<<"}
+              </button>
+              <button
+                onClick={() => previousPage()}
+                disabled={!canPreviousPage}
+              >
+                Previous
+              </button>
+              <span className="pageNumbers">
+                Page{" "}
+                <strong>
+                  {pageIndex + 1} of {pageOptions.length}
+                </strong>{" "}
+              </span>
 
-            <button onClick={() => nextPage()} disabled={!canNextPage}>
-              Next
-            </button>
-            <button
-              onClick={() => gotoPage(pageCount - 1)}
-              disabled={!canNextPage}
-            >
-              {">>"}
-            </button>
+              <button onClick={() => nextPage()} disabled={!canNextPage}>
+                Next
+              </button>
+              <button
+                onClick={() => gotoPage(pageCount - 1)}
+                disabled={!canNextPage}
+              >
+                {">>"}
+              </button>
 
-            <span className="goto">
-              | Go to page
-              <input
-                type="number"
-                defaultValue={pageIndex + 1}
-                onChange={(e) => {
-                  const pageNumber = e.target.value
-                    ? Number(e.target.value) - 1
-                    : 0;
-                  gotoPage(pageNumber);
-                }}
-                style={{ width: "40px" }}
-              />
-            </span>
-          </div>
-        </>
-      )}
-    </div>
+              <span className="goto">
+                | Go to page
+                <input
+                  type="number"
+                  defaultValue={pageIndex + 1}
+                  onChange={(e) => {
+                    const pageNumber = e.target.value
+                      ? Number(e.target.value) - 1
+                      : 0;
+                    gotoPage(pageNumber);
+                  }}
+                  style={{ width: "40px" }}
+                />
+              </span>
+            </div>
+          </>
+        )}
+      </div>
+    </section>
   );
 }
 

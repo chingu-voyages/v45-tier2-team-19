eda8c9e263193315798f635fe5b1a27f557c35fd
Crystal <crwainstock@gmail.com>
2023-08-31 10:27:53 +0200

    update graph styling in summary.css

--- fireball/src/components/summary/StrikesByComposition.jsx
+++ fireball/src/components/summary/StrikesByComposition.jsx
@@ -88,52 +88,53 @@ function StrikesByComposition() {
 
   return (
     <div
-      style={{
-        width: 700,
-        color: "white",
-        marginTop: "0.5rem",
-        background: "rgb(229,231,245)",
-        margin: "0.5rem",
-        // border: "5px solid white",
-        borderRadius: "8px",
-      }}
+      className="strikes-by-composition-container"
+      // style={{
+      //   width: 700,
+      //   color: "white",
+      //   marginTop: "0.5rem",
+      //   background: "rgb(229,231,245)",
+      //   margin: "0.5rem",
+      //   // border: "5px solid white",
+      //   borderRadius: "8px",
+      // }}
     >
       <Line data={chartData} options={chartOptions} />
       {/* <div className="learn-more-container">
         <h4 className="title">Learn more about each meteorite type below</h4>
         <div className="types-of-meteorites-grid">
-          <div className="grid-item">
+          <div className="learn-more-grid-item">
             <a href="https://www.mindat.org/min-49702.html">L5 Meteorites</a>
           </div>
-          <div className="grid-item">
+          <div className="learn-more-grid-item">
             <a href="https://www.mindat.org/min-49619.html">H6 Meteorites</a>
           </div>
-          <div className="grid-item">
+          <div className="learn-more-grid-item">
             <a href="https://www.mindat.org/min-49813.html">EH4 Meteorites</a>
           </div>
-          <div className="grid-item">
+          <div className="learn-more-grid-item">
             <a href="https://www.mindat.org/min-49848.html">
               Acapulcoite Meteorites
             </a>
           </div>
-          <div className="grid-item">
+          <div className="learn-more-grid-item">
             <a href="https://www.mindat.org/min-49710.html">L6 Meteorites</a>
           </div>
-          <div className="grid-item">
+          <div className="learn-more-grid-item">
             <a href="https://www.mindat.org/min-49710.html">LL3-6 Meteorites</a>
           </div>
-          <div className="grid-item">
+          <div className="learn-more-grid-item">
             <a href="https://www.mindat.org/min-49710.html">H5 Meteorites</a>
           </div>
-          <div className="grid-item">
+          <div className="learn-more-grid-item">
             <a href="https://www.mindat.org/min-49710.html">L Meteorites</a>
           </div>
-          <div className="grid-item">
+          <div className="learn-more-grid-item">
             <a href="https://www.mindat.org/min-49710.html">
               Diogenite-pm Meteorites
             </a>
           </div>
-          <div className="grid-item">
+          <div className="learn-more-grid-item">
             <a href="https://www.mindat.org/min-49710.html">H4 Meteorites</a>
           </div>
         </div>

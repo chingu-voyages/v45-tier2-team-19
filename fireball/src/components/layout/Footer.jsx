d9979656b3534694ce0804cfbac6885e6285c8e7
crisxh <76125739+crisxh@users.noreply.github.com>
2023-09-04 11:59:19 +0200

    header top curved

--- fireball/src/components/layout/Footer.jsx
+++ fireball/src/components/layout/Footer.jsx
@@ -3,33 +3,33 @@ import "./Footer.css";
 
 const Footer = function () {
     return (
-        <footer>
-            <div className="footerLogo">
-                <img className="logo" src={logo} alt="header logo" />
-            </div>
-            <div id="madeBy">
-                <h2>App Created By:</h2>
-                <ul>
-                    <a href="https://github.com/vukas86">
-                        <li>Alex</li>
-                    </a>
-
-                    <a href="https://github.com/crwainstock">
-                        <li>Crystal</li>
-                    </a>
-
-                    <a href="https://github.com/crisxh">
-                        <li>Chris</li>
-                    </a>
-                    <a href="https://github.com/uKiJo">
-                        <li>Younes</li>
-                    </a>
-                    <a href="https://github.com/Sarita1517">
-                        <li>Sarita</li>
-                    </a>
-                </ul>
-            </div>
-        </footer>
+        <div id="footerCurvedWrap">
+            <footer>
+                <div className="footerLogo">
+                    <img className="logo" src={logo} alt="header logo" />
+                </div>
+                <div id="madeBy">
+                    <h2>App Created By:</h2>
+                    <ul>
+                        <a href="https://github.com/vukas86">
+                            <li>Alex</li>
+                        </a>
+                        <a href="https://github.com/crwainstock">
+                            <li>Crystal</li>
+                        </a>
+                        <a href="https://github.com/crisxh">
+                            <li>Chris</li>
+                        </a>
+                        <a href="https://github.com/uKiJo">
+                            <li>Younes</li>
+                        </a>
+                        <a href="https://github.com/Sarita1517">
+                            <li>Sarita</li>
+                        </a>
+                    </ul>
+                </div>
+            </footer>
+        </div>
     );
 };
 

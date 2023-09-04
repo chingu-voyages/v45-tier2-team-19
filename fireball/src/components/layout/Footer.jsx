39ca15eb8b918dea4f0e897fa8c6b05a8f9ed70b
crisxh <76125739+crisxh@users.noreply.github.com>
2023-09-04 15:55:01 +0200

    footer content fits down to 450px of width

--- fireball/src/components/layout/Footer.jsx
+++ fireball/src/components/layout/Footer.jsx
@@ -10,26 +10,27 @@ const Footer = function () {
                         <img className="logo" src={logo} alt="header logo" />
                     </div>
                 </div>
-                <div id="madeBy">
+                <div id="madeByContainer">
                     <h2>App Created By:</h2>
-                    <ul>
-                        <a href="https://github.com/vukas86">
-                            <li>Alex</li>
-                        </a>
-                        <a href="https://github.com/crwainstock">
-                            <li>Crystal</li>
-                        </a>
-                        <a href="https://github.com/crisxh">
-                            <li>Chris</li>
-                        </a>
-                        <a href="https://github.com/uKiJo">
-                            <li>Younes</li>
-                        </a>
-                        <a href="https://github.com/Sarita1517">
-                            <li>Sarita</li>
-                        </a>
-                    </ul>
-
+                    <div id="madeBy">
+                        <ul>
+                            <a href="https://github.com/vukas86">
+                                <li>Alex</li>
+                            </a>
+                            <a href="https://github.com/crwainstock">
+                                <li>Crystal</li>
+                            </a>
+                            <a href="https://github.com/crisxh">
+                                <li>Chris</li>
+                            </a>
+                            <a href="https://github.com/uKiJo">
+                                <li>Younes</li>
+                            </a>
+                            <a href="https://github.com/Sarita1517">
+                                <li>Sarita</li>
+                            </a>
+                        </ul>
+                    </div>
                 </div>
             </footer>
         </div>

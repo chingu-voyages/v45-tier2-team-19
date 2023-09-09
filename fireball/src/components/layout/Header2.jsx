80765f15698d44804d365c2d1241a1407334b192
crisxh <76125739+crisxh@users.noreply.github.com>
2023-09-09 13:32:28 +0200

    style

--- fireball/src/components/layout/Header2.jsx
+++ fireball/src/components/layout/Header2.jsx
@@ -12,17 +12,20 @@ const Header2 = function () {
   }, []);
   return (
     <>
-      <header ref={headerRef} className={header.container}>
-        <div className={header.logo}>
-          <img src={logo} alt="logo" />
-        </div>
-        <div>
-          <h1>Title</h1>
-          <p>Description</p>
-        </div>
-        <div className={header.cometContainer}></div>
-      </header>
-      <Navbar headerHeight={headerHeight} />
+      <div className="headerContainer" >
+        <header ref={headerRef} className={header.container}>
+          <div className={header.logo}>
+            <img src={logo} alt="logo" />
+          </div>
+          <div>
+            <h1>Title</h1>
+            <p>Description</p>
+          </div>
+          <div className={header.cometContainer}></div>
+          <Navbar headerHeight={headerHeight} />
+        </header>
+      </div>
+
     </>
   );
 };

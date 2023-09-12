221e835461fdd01904ef5551e46c90d1313d4382
crisxh <76125739+crisxh@users.noreply.github.com>
2023-09-12 12:10:26 +0200

    added some homepages

--- fireball/src/components/layout/MadeBy.jsx
+++ fireball/src/components/layout/MadeBy.jsx
@@ -6,7 +6,9 @@ const MadeBy = function () {
     let creators = [
         {
             name: 'Alex',
-            github: "https://github.com/vukas86"
+            github: "https://github.com/vukas86",
+            linkedIn: "",
+            homePage: "https://game-guess-the-number.vercel.app/",
         },
         {
             name: 'Crystal',
@@ -15,15 +17,21 @@ const MadeBy = function () {
         },
         {
             name: 'Chris',
-            github: "https://github.com/crisxh"
+            github: "https://github.com/crisxh",
+            linkedIn: "https://www.linkedin.com/in/chris-hndx/",
+            homePage: "https://crisxh.github.io/"
         },
         {
             name: 'Younes',
-            github: "https://github.com/uKiJo"
+            github: "https://github.com/uKiJo",
+            linkedIn: "",
+            homePage: "https://www.amyounes.dev/"
         },
         {
             name: 'Sarita',
-            github: "https://github.com/Sarita1517"
+            github: "https://github.com/Sarita1517",
+            linkedIn: "https://www.linkedin.com/in/iqra-qadeer",
+            homePage: "",
         }
 
     ]
@@ -32,7 +40,7 @@ const MadeBy = function () {
         return (
 
             <div id="Credits" className="creator" key={index}>
-                <a href={creator.github}  >
+                <a href={creator.homePage ? creator.homePage : creator.github}  >
                     <li>{creator.name}</li>
                 </a>
                 <a href={creator.linkedIn}><GrLinkedin /></a>

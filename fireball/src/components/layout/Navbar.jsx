48341214e2edd886ab9413ca63c50225bce092b8
crisxh <76125739+crisxh@users.noreply.github.com>
2023-09-12 10:26:51 +0200

    added close button to menu

--- fireball/src/components/layout/Navbar.jsx
+++ fireball/src/components/layout/Navbar.jsx
@@ -1,6 +1,7 @@
 import "./Navbar.css";
 import { useState, useEffect, useRef } from "react";
 import { MdMenu } from 'react-icons/md';
+import { GrClose } from 'react-icons/gr'
 import starryBg from '../../assets/starryMeteorHeader.png'
 
 const Navbar = function ({ headerHeight }) {
@@ -67,7 +68,9 @@ const Navbar = function ({ headerHeight }) {
     {
       name: 'Credits',
       link: "Credits"
-    }
+    },
+
+
   ]
 
 
@@ -106,34 +109,36 @@ const Navbar = function ({ headerHeight }) {
         ) : (
           <div>
             {!toggled || smallScreen ? (
-              <button
-                className='collapsedBtn'
-                onClick={() => { setToggled(!toggled) }}
-                style={sticky ? {
-                  position: 'absolute',
-                  top: '0px',
-                  left: '0px',
-                  fontSize: '2em',
-                  backgroundColor: 'var(--primary)',
-                  width: '100vw'
+              <div>
 
-                } : {
-
-                  fontSize: '5em',
-                  alignSelf: 'center',
-                  justifySelf: 'center',
-                }}><MdMenu /></button>
+                <button
+                  className='collapsedBtn'
+                  onClick={() => { setToggled(!toggled) }}
+                  style={sticky ? {
+                    position: 'absolute',
+                    top: '0px',
+                    left: '0px',
+                    fontSize: '2em',
+                    backgroundColor: 'var(--primary)',
+                    width: '100vw'
+                  } : {
+                    fontSize: '5em',
+                    alignSelf: 'center',
+                    justifySelf: 'center',
+                  }}><MdMenu /></button>
+              </div>
 
             ) : (
               <div style={{
                 display: 'flex',
                 flexDirection: 'column',
                 alignItems: 'center',
-                outline: '2px solid purple'
+
 
               }}>
 
                 <nav  >
+
                   <ul className='collapsedUl' style={{
                     width: '100vw',
                     height: 'fit-content',
@@ -146,6 +151,14 @@ const Navbar = function ({ headerHeight }) {
                     backgroundColor: 'var(--primary)',
 
                   }} >
+                    <button onClick={(() => {
+                      handleMenuClose()
+                    })} className='closeMenu'
+                      style={{
+                        alignSelf: 'flex-end',
+                        fontSize: '2em',
+                        cursor: 'pointer',
+                      }}><GrClose /></button>
                     {menuList.map((item, index) => {
                       return (
                         <a href={`#${item.link}`} onClick={() => { handleMenuClose() }} key={index} >

e411803bbd6e56f9c2981376193bb952a5f568f7
Aleksandar Vukasovic <83580359+vukas86@users.noreply.github.com>
2023-09-13 16:51:48 +0200

    animating logo in footer

--- fireball/src/components/layout/Footer.jsx
+++ fireball/src/components/layout/Footer.jsx
@@ -1,30 +1,36 @@
+import { useRef } from "react";
 
 import logo from "../../assets/logo-no-background.svg";
 import "./Footer.css";
-import MadeBy from './MadeBy';
+import MadeBy from "./MadeBy";
+
+import { useInView } from "framer-motion";
 
 const Footer = function () {
-    return (
-        <div>
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
 
+  return (
+    <div>
+      <footer>
+        <img
+          ref={ref}
+          style={{
+            transform: isInView ? "none" : "translateX(-200px)",
+            opacity: isInView ? 1 : 0,
+            transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s",
+          }}
+          className="footerLogo"
+          src={logo}
+        ></img>
 
-            <footer>
-                <img className='footerLogo' src={logo}></img>
-
-
-                <div className="madeByContainer">
-                    <h2>App Created By:</h2>
-                    <MadeBy />
-                </div>
-
-            </footer>
-
-
+        <div className="madeByContainer">
+          <h2>App Created By:</h2>
+          <MadeBy />
         </div>
-
-    )
-
-
+      </footer>
+    </div>
+  );
 };
 
 export default Footer;

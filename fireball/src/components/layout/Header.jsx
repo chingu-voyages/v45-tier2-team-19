d2d9145f73b3e635c40ad7ec36d2773e352d2229
Aleksandar Vukasovic <83580359+vukas86@users.noreply.github.com>
2023-09-13 12:29:18 +0200

    Animating header title

--- fireball/src/components/layout/Header.jsx
+++ fireball/src/components/layout/Header.jsx
@@ -3,6 +3,8 @@ import logo from "../../assets/Logo.svg";
 import header from "./Header.module.css";
 import Navbar from "./Navbar";
 
+import { motion } from "framer-motion";
+
 const Header = function () {
   const [headerHeight, setHeaderHeight] = useState(0);
   const headerRef = useRef(null);
@@ -10,6 +12,24 @@ const Header = function () {
   useEffect(() => {
     setHeaderHeight(headerRef.current.clientHeight);
   }, []);
+
+  useEffect(() => {
+    const loadingTimeout = setTimeout(() => {
+      setComponIsReady(true);
+    }, 2000);
+    return () => clearTimeout(loadingTimeout);
+  }, []);
+
+  const titleVarients = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1, transition: { delay: 0.2 } },
+  };
+
+  const childVariants = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1, x: 350, transition: { delay: 1.2 } },
+  };
+
   return (
     <>
       <header ref={headerRef} className={header.container}>
@@ -17,9 +37,25 @@ const Header = function () {
           <img src={logo} alt="logo" />
         </div>
         <div className={header.description}>
-          <h2>Your guide on a meteorite journey</h2>
+          {componIsReady && (
+            <motion.h2
+              variants={titleVarients}
+              initial="hidden"
+              animate="visible"
+            >
+              Your guide on a meteorite journey
+            </motion.h2>
+          )}
           <p className={header.p}>
-            Discover meteorite impacts around the world
+            {componIsReady && (
+              <motion.span
+                variants={childVariants}
+                initial="hidden"
+                animate="visible"
+              >
+                Discover meteorite impacts around the world
+              </motion.span>
+            )}
           </p>
         </div>
         <div className={header.cometContainer}></div>

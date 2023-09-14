0948b57e0444e12e9472139a6c5890258a62f618
crisxh <76125739+crisxh@users.noreply.github.com>
2023-09-14 08:06:31 +0200

    commit

--- fireball/src/components/summary/StrikesByMass.jsx
+++ fireball/src/components/summary/StrikesByMass.jsx
@@ -2,68 +2,70 @@ import { useDataContext } from "../../hooks/useDataContext";
 import { useEffect, useState } from "react";
 import './StrikesByMass.css'
 const StrikesByMass = function () {
-    const meteorData = useDataContext().data;
+  const meteorData = useDataContext().data;
 
-    const [meteorState, setMeteorState] = useState(meteorData)
+  const [meteorState, setMeteorState] = useState(meteorData)
 
-    useEffect(() => {
-        setMeteorState(meteorData)
-    }, [meteorData])
-
-
-    let descendingMeteorData = meteorData.sort(function sort(a, b) {
-        return b['mass (g)'] - a['mass (g)']
-    })
-    let ascendingMeteorData = meteorData.sort(function sort(a, b) {
-        return a['mass (g)'] - b['mass (g)']
-    })
+  useEffect(() => {
+    setMeteorState(meteorData)
+  }, [meteorData])
 
 
-    let topTen = descendingMeteorData.slice(0, 10)
-    let bottomTen = ascendingMeteorData.slice(0, 10)
+  let descendingMeteorData = meteorData.sort(function sort(a, b) {
+    return b['mass (g)'] - a['mass (g)']
 
-    const topTenList = topTen.map((meteor, index) => {
-        return (
-            <div className='meteorContainer' key={index}>
-                <p className='meteorName' ><em>Name: </em>{meteor.name}</p> - <p className='meteorMass'><em>Mass: </em>{meteor['mass (g)']}</p>
+  }
 
-            </div>)
+  )
+  let ascendingMeteorData = descendingMeteorData.reverse()
 
 
-    })
+  let topTen = descendingMeteorData.slice(0, 10)
+  let bottomTen = ascendingMeteorData.slice(0, 10)
+  console.log('top and bottom', topTen, bottomTen)
 
-    const bottomTenList = bottomTen.map((meteor, index) => {
-        return (
-            <div className='meteorContainer' key={index}>
-                <p className='meteorName' ><em>Name: </em>{meteor.name}</p> - <p className='meteorMass'><em>Mass: </em>{meteor['mass (g)']}</p>
-
-            </div>)
-
-
-    })
-
-
-
-
-
+  const topTenList = topTen.map((meteor, index) => {
     return (
-        <div className='mass-container  '>
-            <div className="data-text largest-smallest">
+      <div className='meteorContainer' key={index}>
+        <p className='meteorName' ><em>Name: </em>{meteor.name}</p> - <p className='meteorMass'><em>Mass: </em>{meteor['mass (g)']}</p>
 
-                <div className='dataBox'>
-                    <h1>Top Ten</h1>
-                    {topTenList}
+      </div>)
 
-                </div>
 
-                <div className='dataBox'>
-                    <h1>Bottom Ten</h1>
-                    {bottomTenList}
+  })
 
-                </div>
-            </div>
+  const bottomTenList = bottomTen.map((meteor, index) => {
+    return (
+      <div className='meteorContainer' key={index}>
+        <p className='meteorName' ><em>Name: </em>{meteor.name}</p> - <p className='meteorMass'><em>Mass: </em>{meteor['mass (g)']}</p>
+
+      </div>)
+
+
+  })
+
+
+
+
+
+  return (
+    <div className='mass-container  '>
+      <div className="data-text largest-smallest">
+
+        <div className='dataBox'>
+          <h1>Top Ten</h1>
+          {topTenList}
+
         </div>
-    )
+
+        <div className='dataBox'>
+          <h1>Bottom Ten</h1>
+          {bottomTenList}
+
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default StrikesByMass;
